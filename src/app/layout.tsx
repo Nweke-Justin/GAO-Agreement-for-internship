4e544fd2921a3a18e888f3f5dab907086aace3c6
Nweke-Justin <nwekejustin580@gmail.com>
2025-08-27 14:32:31 -0700

    second commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,23 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "GAO's Agreement for Internship",
-  description: "Offer and Agreement Letter",
+  description: "Agreement for Internship",
+  icons:{
+    icon:"./icon.png"
+  }
 };
 
 export default function RootLayout({
@@ -16,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
